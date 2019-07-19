f87d630199846d7f8d6724792cb2c5c8a57d2b31
maribeiro30 <m-antonio-ribeiro@uol.com.br>
2019-07-18 21:23:34 -0300

    Initialize project

---
+++ src/main/java/br/com/criar/corrida/CorridaApplication.java
@@ -0,0 +1,13 @@
+package br.com.criar.corrida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorridaApplication.class, args);
+	}
+
+}
